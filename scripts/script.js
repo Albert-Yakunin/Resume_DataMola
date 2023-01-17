c318f1fd95367d96ef4bb26d78f715f174222d69
Albert <108080795+Albert-Yakunin@users.noreply.github.com>
2023-01-18 01:08:39 +0300

    resume for DataMola

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('.header__menu-icon');
+let navbar = document.querySelector('.navbar__list');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
